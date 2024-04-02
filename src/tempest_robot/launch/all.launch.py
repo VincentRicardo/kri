1fa2659e80ee694832a41711625439963dd5ff4b
VincentRicardo <150823591+VincentRicardo@users.noreply.github.com>
2024-04-02 20:07:17 +0700

    Add files via upload

---
+++ src/tempest_robot/launch/all.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+import launch_ros.actions
+from getkey import getkey, keys
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.BUTTON = 21
+
+GPIO.setup(GPIO.BUTTON, GPIO.IN)
+
+def generate_launch_description():
+    # while True:
+    #     if GPIO.input(GPIO.BUTTON)==1:
+    #         print("Sistem Aktif")
+    #         return LaunchDescription([
+    #             launch_ros.actions.Node(
+    #                 package='tempest_robot', executable = 'teleop_key_servo')
+    #         ])
+    #         break
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package = 'tempest_robot', executable= 'nano0_server'),
+        launch_ros.actions.Node(
+            package = 'tempest_robot', executable = 'nano1_server'),
+        launch_ros.actions.Node(
+            package = 'tempest_robot', executable = 'sending_angle'),
+    ])
