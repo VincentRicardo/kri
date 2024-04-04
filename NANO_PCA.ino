51106c1e84dbb1c0ac25d6735bf99d1884674470
VincentRicardo <150823591+VincentRicardo@users.noreply.github.com>
2024-04-04 18:47:25 +0700

    NANO_PCA.ino

--- NANO_PCA.ino
+++ NANO_PCA.ino
@@ -1,3 +1,4 @@
+#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40); 
 
@@ -5,22 +6,9 @@ Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 #define SERVOMAX  625                                                 // this is the 'maximum' pulse length count (out of 4096)
 
 int sudut[20];
-int StringCount = 0;
-// void seperate_string(String data) {
-//   while (data.length() > 0){
-//     int index = data.indexOf(' ');
-//     if(index == -1)
-//     {
-//       sudut[StringCount++] = data;
-//       break;
-//     }
-//     else
-//     {
-//       sudut[StringCount++] = data.substring(0, index);
-//       data = data.substring(index+1);
-//     }
-//   }
-// }
+unsigned long myTime;
+unsigned long currentTime;
+bool flag =true;
 
 void berdiri(){
   board1.setPWM(0, 0, angleToPulse(60));
@@ -46,44 +34,58 @@ void loop()
   { 
     if (Serial.available() > 0){
       String data = Serial.readStringUntil('\n');
+      Serial.println(data);
       sscanf(data.c_str(), "%d %d %d %d %d %d %d %d %d %d", &sudut[0], &sudut[1], &sudut[2], &sudut[3], &sudut[4], &sudut[5], &sudut[6], &sudut[7], &sudut[8], &sudut[9]);
       if(sudut[0] == 1){
         berdiri();
       }
       else{
         board1.setPWM(1, 0, angleToPulse(170));
-        board1.setPWM(4, 0, angleToPulse(10));
-        board1.setPWM(7, 0, angleToPulse(180));
-        delay(500);
+        board1.setPWM(4, 0, angleToPulse(5));
+        board1.setPWM(7, 0, angleToPulse(175));
+        myTime = millis();
+        while (flag == true){
+          currentTime = millis();
+          if (currentTime - myTime < 500){
+            flag = true;
+          }
+          else if(currentTime - myTime >= 500){
+            flag = false;
+          }
+        }
+        flag = true;
         board1.setPWM(0, 0, angleToPulse(sudut[1]));
         board1.setPWM(3, 0, angleToPulse(sudut[4]));
         board1.setPWM(6, 0, angleToPulse(sudut[7]));
-
-        delay(500);
+        myTime = millis();
+        while (flag == true){
+          currentTime = millis();
+          if (currentTime - myTime < 500){
+            flag = true;
+          }
+          else if(currentTime - myTime >= 500){
+            flag = false;
+          }
+        }
+        flag = true;
         board1.setPWM(1, 0, angleToPulse(105));
         board1.setPWM(4, 0, angleToPulse(70));
         board1.setPWM(7, 0, angleToPulse(110));
-        delay(500);
+        myTime = millis();
+        while (flag == true){
+          currentTime = millis();
+          if (currentTime - myTime < 500){
+            flag = true;
+          }
+          else if(currentTime - myTime >= 500){
+            flag = false;
+          }
+        }
+        flag = true;
         berdiri();
-      }      
-      //StringCount = 0;
-
+      }
+      Serial.write(18);      
     }
-    // board1.setPWM(0, 0, angleToPulse(0));
-    // board1.setPWM(1, 0, angleToPulse(180));
-    // board1.setPWM(2, 0, angleToPulse(180));
-    // board1.setPWM(3, 0, angleToPulse(0));
-    // board1.setPWM(4, 0, angleToPulse(0));    
-    // board1.setPWM(5, 0, angleToPulse(180));
-    //delay(2000);
-    
-    // board1.setPWM(0, 0, angleToPulse(130));
-    // board1.setPWM(1, 0, angleToPulse(180));
-    // board1.setPWM(2, 0, angleToPulse(180));
-    // board1.setPWM(3, 0, angleToPulse(0));
-    // board1.setPWM(4, 0, angleToPulse(0));    
-    // board1.setPWM(5, 0, angleToPulse(0));
-    // delay(2000);
   }
 
 
@@ -91,4 +93,3 @@ int angleToPulse(int ang)                             //gets angle in degree and
   {  int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max 
      return pulse;
   }
-
