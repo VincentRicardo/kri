0b1330a69c0413b2cbe3bd10979130c6ff0346e4
VincentRicardo <150823591+VincentRicardo@users.noreply.github.com>
2024-04-16 16:19:22 +0700

    Add files via upload

---
+++ NANO_PCA.ino
@@ -0,0 +1,164 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
+
+#define SERVOMIN 125  // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 625  // this is the 'maximum' pulse length count (out of 4096)
+
+int sudut[20];
+unsigned long myTime;
+unsigned long currentTime;
+bool flag = true;
+bool check_capit = false;
+
+void tahan_waktu(int waktu) {
+  myTime = millis();
+  while (flag == true) {
+    currentTime = millis();
+    if (currentTime - myTime < waktu) {
+      flag = true;
+    } else if (currentTime - myTime >= waktu) {
+      flag = false;
+    }
+  }
+  flag = true;
+}
+
+void capit() {
+  board1.setPWM(9, 0, angleToPulse(0));
+  tahan_waktu(500);
+  if(check_capit == false){
+    board1.setPWM(10, 0, angleToPulse(5));
+  }else{
+    board1.setPWM(10, 0, angleToPulse(70));
+
+  }
+  tahan_waktu(300);
+  board1.setPWM(9, 0, angleToPulse(90));
+  check_capit = !check_capit;
+}
+
+void berdiri() {
+  board1.setPWM(0, 0, angleToPulse(80));
+  board1.setPWM(1, 0, angleToPulse(90));
+  board1.setPWM(2, 0, angleToPulse(140));
+  board1.setPWM(3, 0, angleToPulse(65));
+  board1.setPWM(4, 0, angleToPulse(95));
+  board1.setPWM(5, 0, angleToPulse(15));
+  board1.setPWM(6, 0, angleToPulse(40));
+  board1.setPWM(7, 0, angleToPulse(110));
+  board1.setPWM(8, 0, angleToPulse(180));
+}
+
+void buka() {
+  board1.setPWM(0, 0, angleToPulse(80));
+  board1.setPWM(1, 0, angleToPulse(110));
+  board1.setPWM(2, 0, angleToPulse(140));
+  board1.setPWM(3, 0, angleToPulse(65));
+  board1.setPWM(4, 0, angleToPulse(75));
+  board1.setPWM(5, 0, angleToPulse(15));
+  board1.setPWM(6, 0, angleToPulse(40));
+  board1.setPWM(7, 0, angleToPulse(130));
+  board1.setPWM(8, 0, angleToPulse(180));
+}
+
+void stand_awal() {
+  //lengan & capit
+  board1.setPWM(9, 0, angleToPulse(90));
+  board1.setPWM(10, 0, angleToPulse(70));
+
+  //Coxa
+  board1.setPWM(0, 0, angleToPulse(80));
+  board1.setPWM(3, 0, angleToPulse(65));
+  board1.setPWM(6, 0, angleToPulse(40));
+
+  //femur naik dlu
+  board1.setPWM(1, 0, angleToPulse(160));
+  board1.setPWM(4, 0, angleToPulse(20));
+  board1.setPWM(7, 0, angleToPulse(160));
+
+  //tibia turun paling bawah
+  board1.setPWM(2, 0, angleToPulse(140));
+  board1.setPWM(5, 0, angleToPulse(15));
+  board1.setPWM(8, 0, angleToPulse(180));
+
+  tahan_waktu(700);
+  //femur posisi berdiri
+  board1.setPWM(1, 0, angleToPulse(90));
+  board1.setPWM(4, 0, angleToPulse(95));
+  board1.setPWM(7, 0, angleToPulse(110));
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  board1.begin();
+  board1.setPWMFreq(60);
+  stand_awal();
+}
+
+
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.println(data);
+    sscanf(data.c_str(), "%d %d %d %d %d %d %d %d %d %d", &sudut[0], &sudut[1], &sudut[2], &sudut[3], &sudut[4], &sudut[5], &sudut[6], &sudut[7], &sudut[8], &sudut[9]);
+    if (sudut[0] == 1) {
+      berdiri();
+      Serial.write(18);
+    } else if (sudut[0] == 0) {
+      Serial.write(18);
+      board1.setPWM(1, 0, angleToPulse(160));
+      board1.setPWM(4, 0, angleToPulse(20));
+      board1.setPWM(7, 0, angleToPulse(160));
+
+      tahan_waktu(400);
+
+      board1.setPWM(0, 0, angleToPulse(sudut[1]));
+      board1.setPWM(3, 0, angleToPulse(sudut[4]));
+      board1.setPWM(6, 0, angleToPulse(sudut[7]));
+
+      tahan_waktu(100);
+
+      board1.setPWM(1, 0, angleToPulse(sudut[2]));
+      board1.setPWM(4, 0, angleToPulse(sudut[5]));
+      board1.setPWM(7, 0, angleToPulse(sudut[8]));
+
+      tahan_waktu(600);
+      berdiri();
+    } else if (sudut[0] == 2) {
+      buka();
+      Serial.write(18);
+    }else if (sudut[0] == 4){
+      capit();
+      Serial.write(18);
+    }else if (sudut[0] == 3){
+      Serial.write(18);
+      board1.setPWM(1, 0, angleToPulse(160));
+      board1.setPWM(4, 0, angleToPulse(20));
+      board1.setPWM(7, 0, angleToPulse(160));
+
+      tahan_waktu(400);
+
+      board1.setPWM(0, 0, angleToPulse(sudut[1]));
+      board1.setPWM(3, 0, angleToPulse(sudut[4]));
+      board1.setPWM(6, 0, angleToPulse(sudut[7]));
+
+      tahan_waktu(100);
+
+      board1.setPWM(1, 0, angleToPulse(sudut[2]));
+      board1.setPWM(4, 0, angleToPulse(sudut[5]));
+      board1.setPWM(7, 0, angleToPulse(sudut[8]));
+
+      tahan_waktu(600);
+      buka();
+    }
+  }
+}
+
+
+int angleToPulse(int ang)  //gets angle in degree and returns the pulse width
+{
+  int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max
+  return pulse;
+}
